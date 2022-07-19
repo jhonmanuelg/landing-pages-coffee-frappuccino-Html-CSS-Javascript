e050e9cff1e39957c83be4685ca63e4563c12a24
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-07-19 15:54:24 -0300

    Subindo: Javascript Landing Page Website

---
+++ js/coffee.js
@@ -0,0 +1,11 @@
+// coffee selection
+function igSmlider(anything){
+    document.querySelector('.Starbucks').src = anything;
+
+}
+// to change the brown background colors
+function chagenCircleColor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+
+}
